c210164c3f511405966477d5cd4678ff8fcf7786
Nsikak Akpan <builtybykaksy@gmail.com>
2023-09-14 18:27:10 +0100

    new update
    Complete Login/signin Page

--- src/pages/signinpage/Signinpage.jsx
+++ src/pages/signinpage/Signinpage.jsx
@@ -5,37 +5,26 @@ import { auth } from '../../firebase';
 import { setUserName } from '../../features/userSlice';
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router';
-import { createUserWithEmailAndPassword} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import './signinpage.css'
 
 const Signinpage = () => {
-  const [formData, setformData] = useState({email:"", password:"", passwordCheck:""}); 
+  const [formData, setformData] = useState({ email:"", password:"" });
   const [type, setType] = useState('password');
-  const [confirmType, setConfirmType] = useState('password');
-  const [strengthLabel, setStrengthLabel] = useState('');
-  const [strengthWidth, setStrengthWidth] = useState('0%');
-  const [background, setBackground] = useState('');
-  const [textColor, setTextColor] = useState('')
-  const dispatch = useDispatch();
-  const navigate = useNavigate(); 
-  const indicatorTextElRef = useRef(null);
-  const indicatorStrenghtElRef = useRef(null);
+  const [signUp, setSignUp] = useState(false);
+  // const dispatch = useDispatch()
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    calculatePasswordStrength(formData.password)
-  }, [formData.password])
-
-  //Function for updating state of the form
   const handleChange = (e) => {
     const {name, value} = e.target;
     setformData(prevData => {
       return {
         ...prevData,
         [name]: value
-      }      
-    }) 
+      }
+    })    
   }
 
   //Function for Hiding/Revealing Password
@@ -51,79 +40,21 @@ const Signinpage = () => {
 
   //Function for signing up user with google firebase authentication
   const signInUser = (e) => {
-    if(formData.password !== formData.passwordCheck){
-      alert('Passwords do not match');
-    }else{
-      e.preventDefault();
-      // dispatch(setUserName(formData.username))
-      createUserWithEmailAndPassword(auth, formData.email, formData.password)
-        .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        navigate("/movies");        
-        // ...
-        })
-        .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(errorMessage)
-      // ..
-      });
-    }   
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, formData.email, formData.password)
+    .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    navigate("/movies");  
+    // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      // navigate("/"); 
+    });
   }
-
-  //This function uses RegEx to calculate password strength
-  function calculatePasswordStrength(password) {
-    const lengthScore = password.length >= 6 ? 10 : 0;
-    const lowercaseRegex = /[a-z]/g;
-    const uppercaseRegex = /[A-Z]/g;
-    const numericRegex = /[0-9]/g;
-    const specialRegex = /[^a-zA-Z0-9\s]/g;
-    const characterScores = {
-      lowercase: password.match(lowercaseRegex) ? 20 : 0,
-      uppercase: password.match(uppercaseRegex) ? 20 : 0,
-      numeric: password.match(numericRegex) ? 20 : 0,
-      special: password.match(specialRegex) ? 20 : 0,
-    };
-    const repeatedCharsRegex = /(.)\1/g;
-    const repeatedCharsScore = password.match(repeatedCharsRegex) ? -10 : 0;
-  
-    const strength = lengthScore +
-      characterScores.lowercase +
-      characterScores.uppercase +
-      characterScores.numeric +
-      characterScores.special +
-      repeatedCharsScore;
-  
-    const mappedStrength = Math.max(Math.floor((strength / 90) * 100), 0);
-    console.log('Strength >>>', strength, 'mapped-Strength >>>', mappedStrength);
-    getStrengthLabel(mappedStrength);       
-    setStrengthWidth(`${mappedStrength}%`);
-    return mappedStrength;
-  }
-
-  //this function updates the strength indicator as appropriate.
-  const getStrengthLabel = (mappedStrength) => {
-    if (mappedStrength <= 25) {
-        setTextColor('#ff0000');
-        setBackground('#ff0000');
-        setStrengthLabel('Too Weak');      
-    } else if (mappedStrength <= 50) {
-        setTextColor('#ff4757');
-        setBackground('#ff4757');
-        setStrengthLabel('Weak');      
-    } else if (mappedStrength <= 75) {
-        setTextColor('#ffa500');
-        setBackground('#ffa500');
-        setStrengthLabel('Strong');      
-    } else {
-        setTextColor('#008000');
-        setBackground('#008000');
-        setStrengthLabel('Very Strong');      
-    }
-  };
-
-
 
   return (
     <div className="signup-page">
